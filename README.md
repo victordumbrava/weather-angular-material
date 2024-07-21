61610cccf8ee7c445d8dee6972d2fff4703f17d1
Victor Dumbravă <victordumbrava91@gmail.com>
2024-07-21 18:37:11 +0300

    Init weather component to display summary info

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.1.1.
 
+## Weather API sources
+https://www.tomorrow.io/weather-api/
+https://open-meteo.com/
+https://openweathermap.org/api
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
---
+++ src/app/components/weather-summary/weather-summary.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-weather-summary',
+    standalone: true,
+    templateUrl: './weather-summary.component.html',
+    styleUrls: ['./weather-summary.component.scss'],
+    imports: [
+
+      ]
+})
+export class WeatherSummaryComponent {
+
+    // get localisation
+    // try get weather info from api
+    // 
+    /*
+    display summary
+        location    
+        temperature
+        wind
+        humidity
+    */
+
+}
